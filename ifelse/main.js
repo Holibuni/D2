3840f2b8b03e6e39219c65a34e84ec0acde76824
Cloé <cloeadeline10@gmail.com>
2025-06-23 15:59:24 +0200

    giblidule

---
+++ ifelse/main.js
@@ -0,0 +1,126 @@
+console.log("coucou");
+
+// let age = prompt("Quel est votre âge?");
+// console.log(age);
+
+// if (age < 13) {
+//   console.log("Tu es un enfant");
+// } else if (age >= 13 && age < 17) {
+//   console.log("Tu es un adolescent");
+// } else if (age >= 17 && age < 59) {
+//   console.log("Tu es un adulte");
+// } else if (age == 100) {
+//   console.log("Wow ! Tu as un siècle !");
+// } else if (age >= 60 && age > 1200) {
+//   console.log("Tu es une personne âgée");
+// } else {
+//   console.log("Age invalide");
+// }
+
+// let jour = prompt("Saisissez un jour de la semaine");
+// console.log(jour);
+
+// switch (jour) {
+//   case "Lundi":
+//     console.log("C'est le début de la semaine");
+//     break;
+
+//   case "Mardi":
+//     console.log("Courage, c'est mardi !");
+//     break;
+
+//   case "Mercredi":
+//     console.log("C'est le milieu de la semaine.");
+//     break;
+
+//   case "Jeudi":
+//     console.log("Bientôt le week-end !");
+//     break;
+
+//   case "Vendredi":
+//     console.log("Dernier jour avant le week-end !");
+//     break;
+
+//   case "Samedi":
+//   case "Dimanche":
+//     console.log("C'est le week-end.");
+
+//   default:
+//     console.log("Jour invalide");
+// }
+
+// let temperature = 25;
+// let statut = temperature >= 20 ? "Il fait chaud" : "Il fait froid";
+// console.log(statut);
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let nombre = 1;
+// do {
+//   console.log(nombre);
+//   nombre++;
+// } while (nombre < 5);
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// function salut(prenom) {
+//   console.log(`Bonjour ${prenom}`);
+// }
+
+// salut("Roger");
+// salut("Clotaire");
+
+// function addition(a, b) {
+//   return a + b;
+// }
+// let tata = addition(15, 15);
+// console.log(tata);
+
+// function afficherMessage() {
+//   let variablelocale = "Caché";
+//   console.log(variablelocale);
+// }
+// afficherMessage();
+
+// let addition = (a, b) => a + b;
+// console.log("Résultat fléché :", addition(89, 73));
+
+// function compteARebours(n) {
+//   if (n == 0) {
+//     return;
+//   } else {
+//     console.log(n);
+//     compteARebours(n - 1);
+//   }
+// }
+// console.log("Compte à Rebours depuis 5:");
+// compteARebours(5);
+
+function choixOrdinateur(pierre, feuille, ciseaux) {
+  let choix = [pierre, feuille, ciseaux];
+  return choix;
+}
+let choix = choixOrdinateur("pierre", "feuille", "ciseaux");
+console.log(choixOrdinateur);
