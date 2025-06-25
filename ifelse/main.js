6f95cce119004ca13893bb22e3706b8511be4d4f
Holibuni <cloeadeline10@gmail.com>
2025-06-25 23:07:26 +0200

    aïe

--- ifelse/main.js
+++ ifelse/main.js
@@ -118,9 +118,20 @@ console.log("coucou");
 // console.log("Compte à Rebours depuis 5:");
 // compteARebours(5);
 
-function choixOrdinateur(pierre, feuille, ciseaux) {
-  let choix = [pierre, feuille, ciseaux];
-  return choix;
+function choixOrdinateur(...choix) {
+  if (choix.length === 0) {
+    throw new Error("Aucun choix fourni.");
+  }
+  return choix[Math.floor(Math.random() * choix.length)];
 }
 let choix = choixOrdinateur("pierre", "feuille", "ciseaux");
-console.log(choixOrdinateur);
+console.log("Choix de l'ordinateur :", choix);
+
+function determinerGagnant(joueur, ordinateur) {
+  // DeterminerGagnant, ce serait des if pour les différents cas, check si le choixOrdinateur === le choix du joueur, donc une égalité, puis déterminer quel genre, "if choixJoueur === ciseau && choixOrdinateur === feuille alors victoire"
+}
+
+function jouerShifumi() {
+  prompt("Choisissez entre pierre, feuille ou ciseaux");
+  let choix = prompt("Choisissez entre pierre, feuille ou ciseaux");
+}
